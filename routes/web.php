efc2d2d086ed58c1f8c7cbc1cf4eee34cbfe3d2a
ScriptSinger <ufamasters102@gmail.com>
2026-05-31 14:59:12 +0500

    feat: moonshine install

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,22 @@
 <?php
 
 use App\Jobs\TestQueueJob;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     TestQueueJob::dispatch();
     return view('welcome');
+});
+
+Route::get('/bybit-test', function () {
+
+    $response = Http::withHeaders([
+        'X-BAPI-API-KEY' => env('BYBIT_API_KEY'),
+    ])->get('https://api.bybit.com/v5/market/tickers', [
+        'category' => 'spot',
+        'symbol' => 'BTCUSDT'
+    ]);
+
+    return $response->json();
 });
